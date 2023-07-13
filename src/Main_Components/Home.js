9b5bb27045204963511e87404300e8e6f118300f
George10He <109994515+George10H@users.noreply.github.com>
2023-07-13 09:58:13 -0400

    j

--- src/Main_Components/Home.js
+++ src/Main_Components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Section_1 from "../Sub_components/section_1";
 import Section_2 from "../Sub_components/section_2";
+import Section_3 from "../Sub_components/section_3";
 
 function Home() {
   return (
     <>
       <Section_1 />
       <Section_2 />
+      <Section_3/>
     </>
   );
 }
---
+++ src/Sub_components/section_3.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../Styles/Section_3.css";
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Section_3() {
+  return (
+    <>
+      <section id="sec3">
+        <div className="left-title">
+          <p>Find the right job or internship for you</p>
+        </div>
+        <div className="right-content">
+          <div className="rc-title">
+            <p>SUGGESTED SEARCHES</p>
+          </div>
+          <div className="rc-lst">
+            <div className="rc-lst-ul">
+              <div>Finance</div>
+              <div>Business Development</div>
+              <div>Engineering</div>
+              <div>Administrative Assistant</div>
+              <div>Retail Associate</div>
+              <div>Customer Service</div>
+              <div>Operations</div>
+              <div>Information Technology</div>
+              <div>Marketing</div>
+              <div>Customer Service</div>
+              <div>Human Resources</div>
+              <div>Military and Protective Services</div>
+            </div>
+          </div>
+          <div className="rc-btn">
+            <button>Show More</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Section_3;
