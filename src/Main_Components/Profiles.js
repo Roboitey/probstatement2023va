7d150b3987722094b0d0d44c52c8d315f400749e
George10He <109994515+George10H@users.noreply.github.com>
2023-07-15 14:05:17 -0400

    n

--- src/Main_Components/Profiles.js
+++ src/Main_Components/Profiles.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import { getProfile } from "../services/profileService";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -12,11 +13,35 @@ function Profiles() {
   }, []);
   return (
     <>
-      <div>Profiles {user["user_id"]}</div>
-      <div>Profiles {user["username"]}</div>
-      <div>Profile email: {user["email"]}</div>
+      <div className="profile-section-container">
+        <div className="ps-profile-card">
+          <img className="pc-back-img" src="#" alt="Not Found" />
+          <img className="pc-profile-img" src="#" alt="Not Found" />
+          <div className="pc-edit">
+            <ModeEditOutlineOutlinedIcon />
+          </div>
+          <div className="pc-information">
+            <div className="pc-info-name">
+              <h1>{user["username"]}</h1>
+            </div>
+            <div className="pc-info-email/type">
+              <div className="pc-info-email">
+                <p>Email Address: {user["email"]}</p>
+              </div>
+              <div className="pc-info-type">
+                <p>{user["type"]}</p>
+              </div>
+            </div>
+            <div className="pc-info-views">
+              <p>{user["views"]}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*<div>Profiles {user["user_id"]}</div>
       <div>Profile type: {user["type"]}</div>
       <div>Profile about: {user.sections?.about}</div>
+  */}
     </>
   );
 }
