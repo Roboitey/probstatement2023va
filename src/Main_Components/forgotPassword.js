c27714fd7f9cf7f76ff71e31855652a6470a228f
Hasanat Shakir <shakirhasanat@gmail.com>
2023-07-15 14:11:39 -0400

    Update forgotPassword.js

--- src/Main_Components/forgotPassword.js
+++ src/Main_Components/forgotPassword.js
@@ -4,24 +4,45 @@ import { useState } from 'react';
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
-  const [sumbit, setSumbit] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [submit, setSubmit] = useState(false);
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("Recovery email has been sent to " + email);
-    setSumbit(true)
+    if(submit){
+      if(password !== confirmPassword){
+        setError("password does not match")
+      } else{
+        console.log('Password changed');
+      }
+    } else {
+      console.log("Recovery email has been sent to " + email);
+      setSubmit(true)
+    }
   }
   return (
-    (!sumbit)? (
-    <div className="forget-pass">
-      <h1>Forgot Password?</h1>
-      <form onSubmit={handleSubmit}>
-        <label for="email" placeholder="enter...">Email:</label>
-        <input type="email" id="email" name="email" placeholder='email: ' onChange={(e) => { setEmail(e.target.value) }} />
-        <button type="submit">Reset Password</button>
-      </form>
-    </div>) :(
-    <div>Set Password</div>
-    //To-do: add form to set a new password
+    (!submit) ? (
+      <div className="forget-pass">
+        <h1>Forgot Password?</h1>
+        <form onSubmit={handleSubmit}>
+          <label for="email" placeholder="enter...">Email:</label>
+          <input type="email" id="email" name="email" placeholder='email: ' onChange={(e) => { setEmail(e.target.value) }} />
+          <button type="submit">Reset Password</button>
+        </form>
+      </div>
+    ) : (
+      <div className="forget-pass">
+        <h1>Set New Password</h1>
+        <form onSubmit={handleSubmit}>
+          <label for="password" placeholder="enter...">Password:</label>
+          <input type="password" id="password" name="password" placeholder='password: ' onChange={(e) => { setPassword(e.target.value) }} />
+          <label for="confirmPassword" placeholder="enter...">ConfirmPassword:</label>
+          <input type="confirmPassword" id="confirmPassword" name="confirmPassword" placeholder='confirmPassword: ' onChange={(e) => { setconfirmPassword(e.target.value) }} />
+          {error}
+          <button type="submit">Reset Password</button>
+        </form>
+      </div>
     )
   )
 }
