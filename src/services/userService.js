aa222270d14359d4f741b9621030385751aa4eb0
George10He <109994515+George10H@users.noreply.github.com>
2023-07-29 11:16:41 -0400

    Profile section

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,42 @@
-export function getUser (Username){
-    const requestOptions = {
-        method: "get", 
-        headers: {
-            "Content-Type": "application/json", 
-            "Authorization": "Bearer 0986ce94-fa35-49b9-a7cc-a631322aa384"
-        }
-    }
-    return fetch ("https://inbdpa.api.hscc.bdpa.org/v1/users/" + Username, requestOptions)
-        .then(data => data.json())
+export function getUser(Username) {
+  const requestOptions = {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer 0986ce94-fa35-49b9-a7cc-a631322aa384",
+    },
+  };
+  return fetch(
+    "https://inbdpa.api.hscc.bdpa.org/v1/users/" + Username,
+    requestOptions
+  ).then((data) => data.json());
+}
+export function UserEdit(
+  user_id,
+  about,
+  Experience,
+  Education,
+  Volunteering,
+  Skills
+) {
+  const requestOptions = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer 0986ce94-fa35-49b9-a7cc-a631322aa384",
+    },
+    body: JSON.stringify({
+      sections: {
+        about: about,
+        experience: [Experience]    ,
+        education: [Education],
+        volunteering: [Volunteering],
+        skills: [Skills],
+      },
+    }),
+  };
+  return fetch(
+    "https://inbdpa.api.hscc.bdpa.org/v1/users/" + user_id,
+    requestOptions
+  ).then((data) => data.json());
 }
