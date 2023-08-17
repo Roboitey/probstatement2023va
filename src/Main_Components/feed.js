d83fa561a1c76e68a2058f68b2d866addfe80112
Roboitey <Islerih25@gmail.com>
2023-08-17 15:56:59 -0400

    Articles and Pagination (Un Working Search Bar)
    ....

--- src/Main_Components/feed.js
+++ src/Main_Components/feed.js
@@ -1,22 +1,70 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getArticles } from "../services/feedService";
 import "../Styles/Feed.css";
 
 function Feed() {
   const [articles, setArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [articlesPerPage] = useState(3); // Adjust the number of articles per page
+  const [totalPages, setTotalPages] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
+    // Replace this with your actual fetch function
+    // For the sake of example, let's assume getArticles() returns a promise
     getArticles().then((data) => {
       console.log(data.articles);
       setArticles(data.articles);
+      setTotalPages(Math.ceil(data.articles.length / articlesPerPage));
     });
-  }, []);
+  }, [articlesPerPage]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const searchPaginationItems = (e) => {
+    setSearchTerm(e.target.value)
+    setCurrentPage(1)
+  }
+
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = articles.slice(indexOfFirstArticle, indexOfLastArticle);
+
 
   return (
     <>
-      {articles.map((item, key) => {
-        return (
-          <>
+    <div className="opportunity-container">
+      <div className="opportunity-header">
+        <h2 classname="opportunity-title">
+          Articles:
+         
+        </h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="opportunity-search"
+          value={searchTerm}
+          onChange={searchPaginationItems}
+        />
+      </div>
+     <div classname="opportunity-content">
+     {currentArticles.length === 0 ? (
+          <div className="no-results">No results found...</div>
+        ) : (
+          currentArticles.map((item, key) => (
             <div className="article-cont" key={key}>
               <div className="article-top">
                 <div className="article-title">
@@ -39,14 +87,36 @@ function Feed() {
                 </div>
               </div>
             </div>
-          </>
-        );
-      })}
+          ))
+        )}
+     </div>
+    </div>
+      
+
+      <div className="pagination">
+        <button className="pagination-button" onClick={goToPreviousPage}>
+          {"<"}
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? "pagination-button active" : "pagination-button"}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button className="pagination-button" onClick={goToNextPage}>
+          {">"}
+        </button>
+      </div>
+
       <div className="create-article-btn">
         <button>Create Article</button>
       </div>
     </>
   );
+  
 }
 
 export default Feed;
